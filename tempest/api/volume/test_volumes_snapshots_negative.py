365b81b0bd24b45e36924c87042112fb4230c961
Andrew Kerr <andrew.kerr@netapp.com>
2013-12-04 15:42:03 -0500

    Further bug fixes for test_delete_volume_with_dependent_snapshot

--- tempest/api/volume/test_volumes_snapshots_negative.py
+++ tempest/api/volume/test_volumes_snapshots_negative.py
@@ -22,6 +22,12 @@ from tempest.test import attr
 
 class VolumesSnapshotNegativeTest(base.BaseVolumeTest):
     _interface = "json"
+    
+    @classmethod
+    def setUpClass(cls):
+        super(VolumesSnapshotNegativeTest, cls).setUpClass()
+        cls.vol_client = cls.volumes_client
+        cls.snap_client = cls.snapshots_client
 
     @attr(type=['negative', 'gate'])
     def test_create_snapshot_with_nonexistent_volume_id(self):
@@ -46,19 +52,20 @@ class VolumesSnapshotNegativeTest(base.BaseVolumeTest):
         v_name = data_utils.rand_name('Volume-')
         s_name = data_utils.rand_name('Snap-')
         # Create volume
-        resp, volume = self.volumes_client.create_volume(size=1,
+        resp, volume = self.vol_client.create_volume(size=1,
                                                  display_name=v_name)
         self.assertEqual(200, resp.status)
         self.assertIn('id', volume)
-        self.addCleanup(self.volumes_client.delete_volume, volume['id'])
-        self.volumes_client.wait_for_volume_status(volume['id'], 'available')
+        self.addCleanup(self.vol_client.delete_volume, volume['id'])
+        self.vol_client.wait_for_volume_status(volume['id'], 'available')
         # Create snapshot
-        resp, snapshot = self.snapshots_client.create_snapshot(volume['id'],
+        resp, snapshot = self.snap_client.create_snapshot(volume['id'],
                                                      display_name=s_name)
         self.assertEqual(200, resp.status)
         self.assertIn('id', snapshot)
-        self.addCleanup(self.snapshots_client.delete_snapshot, snapshot['id'])
-        self.assertRaises(exceptions.BadRequest, self.volumes_client.delete_volume, '')
+        self.addCleanup(self.snap_client.delete_snapshot, snapshot['id'])
+        self.snap_client.wait_for_snapshot_status(snapshot['id'], 'available')
+        self.assertRaises(exceptions.BadRequest, self.vol_client.delete_volume, '')
 
 
 class VolumesSnapshotNegativeTestXML(VolumesSnapshotNegativeTest):
